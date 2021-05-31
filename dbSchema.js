1ce60a4032e95cc593ead204b214328588ad589f
Sabri0o <sabritrabelsi90@gmail.com>
2021-05-31 15:02:30 +0100

    creating schema and connecting db

---
+++ dbSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+let IssueTracker;
+
+//creating tracker schema
+
+const issueTracker = new mongoose.Schema({
+    "issue_title":{type:String,require:true},
+    "issue_text":{type:String,required:true},
+    "created by":{type:String,required:true},
+    "assigned_to":{type:String},
+    "open":{type:Boolean,default:true},
+    "status_text":{type:String}
+},{timestamps:{createdAt:"created_on",updatedAt:"updated_on"}})
+
+// creating a model
+IssueTracker = mongoose.model("IssueTracker",issueTracker)
+
+exports.IssueTrackerModel = IssueTracker
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 require('dotenv').config()
-
+const mongoose = require("mongoose")
 
 // to reconize the request object as json request
 app.use(express.json())
@@ -14,6 +14,20 @@ app.use('/public',express.static(__dirname + "/public"));
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/views/index.html')
 })
+
+// connecting database
+const mySecret = process.env.MONGO_URI
+mongoose.connect(mySecret,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+},function(error){
+    if(error){
+        console.log("Database error or database connection error " + error)
+    }
+    console.log("Database state is " + !!mongoose.connection.readyState)
+})
+
 
 app.listen(process.env.PORT || 8000,()=>{
     console.log('server is listening...')
