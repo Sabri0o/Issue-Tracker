78b43866918dd390c17148fa656ccc2b39419979
Sabri0o <sabritrabelsi90@gmail.com>
2021-06-03 12:38:14 +0100

    find a way to run the test without exceeding time/need to create new collection for testing only

--- tests/functional_tests.js
+++ tests/functional_tests.js
@@ -6,7 +6,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const expect = require("chai").expect;
 
-
 chai.use(chaiHttp);
 
 // Create an issue with every field: POST request to /api/issues/{project}
@@ -23,43 +22,71 @@ chai.use(chaiHttp);
 // Delete an issue: DELETE request to /api/issues/{project}
 // Delete an issue with an invalid _id: DELETE request to /api/issues/{project}
 // Delete an issue with missing _id: DELETE request to /api/issues/{project}
-
 describe("Functional tests", function () {
-  before(function (done) {
-    const mySecret = process.env.MONGO_URI;
-    mongoose.connect(
-      mySecret,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      },
-      function (error) {
-        if (error) {
-          console.log("Database error or database connection error " + error);
-        }
-        console.log("connected to test database!");
-        done();
-      }
-    );
-  });
-  describe("POST request to /api/issues/{project}", function () {
+  //Before starting the test, create a sandboxed database connection
+  //Once a connection is established invoke done()
+  const mySecret = process.env.MONGO_URI;
+
+  // before(function (done) {
+  //   // https://github.com/mochajs/mocha/issues/2025
+  //   // increasing the timeout to longer than the HTTP API is expected to take to respond
+  //   this.timeout(10000);
+
+  //   mongoose.connect(
+  //     mySecret,
+  //     {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true,
+  //       useFindAndModify: false,
+  //     },
+  //     function (error) {
+  //       if (error) {
+  //         console.log("Database error or database connection error " + error);
+  //       }
+  //       console.log("Database state is " + !!mongoose.connection.readyState);
+  //       done();
+  //     }
+  //   );
+  // });
+  describe("Post requests tests", function () {
     it("should create an issue with every field: POST request to /api/issues/{project}", function (done) {
+      //increasing the timeout to longer than the HTTP API is expected to take to respond
+      this.timeout(10000);
       chai
         .request(server)
         .post("/api/issues/project_XY/")
         .send({
-          title: "ticket_5",
+          title: "ticket_X",
           text: "testing",
           createdBy: "sabri0o",
           assignedTo: "mister X",
           statusText: "kill everyone then kill yourself",
         })
         .end((err, res) => {
-          console.log(res.text);
-          expect({a: 25}).to.deep.equal({a: 1})
+          console.log("res.body", res.body);
+
+          expect(res.status).to.equal(200);
+          expect(res.body[0]).to.have.property("open", true);
+
+          expect(res.body[0]).to.have.property("issue_title", "ticket_X");
+          expect(res.body[0]).to.have.property("issue_text", "testing");
+          expect(res.body[0]).to.have.property("created_by", "sabri0o");
+
+          expect(res.body[0]).to.have.property("assigned_to", "mister X");
+          expect(res.body[0]).to.have.property(
+            "status_text",
+            "kill everyone then kill yourself"
+          );
+          expect(res.body[0]).to.have.property("created_on");
+          expect(res.body[0]).to.have.property("updated_on");
+
           done();
         });
     });
+  });
+  //After all tests are finished close connection
+  after(function (done) {
+    console.log("disconnecting db");
+    mongoose.connection.close(done);
   });
 });
